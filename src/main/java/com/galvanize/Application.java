882a97a4cedf8767184fbf24603cf2beb483dc6b
“adaehler” <antonio_daehler@yahoo.com>
2020-11-30 09:39:42 -0800

    workings?

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,7 +3,8 @@ package com.galvanize;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hello world.Is this working?..not working.");
+        System.out.println("hello world.Is this working?..not working." +
+                "+ never understood if this is working, goin to keep trying");
     }
 
 }
